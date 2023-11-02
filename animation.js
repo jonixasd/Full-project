169d92e3247ee525d1573160d1e3bab1e46bf196
jonixasd <k94923k@gmail.com>
2023-11-03 01:20:56 +0800

    2023-11-03_01-20

--- animation.js
+++ animation.js
@@ -4,3 +4,26 @@ window.addEventListener('scroll', function () {
   var stickyNavbar = document.querySelector('header');
   stickyNavbar.classList.toggle('sticky', window.scrollY > 0);
 });
+
+// Owlcarousel
+
+// $(document).ready(function () {
+//   $('.owl-carousel').owlCarousel();
+// });
+
+// $('.owl-carousel').owlCarousel({
+//   loop: true,
+//   margin: 10,
+//   nav: true,
+//   responsive: {
+//     0: {
+//       items: 1,
+//     },
+//     600: {
+//       items: 3,
+//     },
+//     1000: {
+//       items: 5,
+//     },
+//   },
+// });
