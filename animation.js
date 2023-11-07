c968274b0b8763b4eecae55322b5266cd6c895d7
jonixasd <k94923k@gmail.com>
2023-11-07 16:46:14 +0800

    2023-11-07_04-46

--- animation.js
+++ animation.js
@@ -29,3 +29,22 @@ function hideDropdown() {
 function cartAlert() {
   alert('已收到你的報名資訊，感謝你~');
 }
+
+//Owl carousel
+
+$('.owl-carousel').owlCarousel({
+  loop: true, // 循環播放
+  margin: 10, // 外距 10px
+  nav: true, // 顯示點點
+  responsive: {
+    0: {
+      items: 1, // 螢幕大小為 0~600 顯示 1 個項目
+    },
+    600: {
+      items: 3, // 螢幕大小為 600~1000 顯示 3 個項目
+    },
+    1000: {
+      items: 5, // 螢幕大小為 1000 以上 顯示 5 個項目
+    },
+  },
+});
