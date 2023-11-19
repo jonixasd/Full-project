ac312f8d813c21cd4049848bfcdeda2ed99e9589
jonixasd <k94923k@gmail.com>
2023-11-20 01:04:03 +0800

    2023-11-20_0104

--- main.js
+++ main.js
@@ -1,22 +1,3 @@
-// nav responsive
-function showDropdown() {
-  const Dropdown = document.querySelector('.dropdown_menu');
-  Dropdown.style.display = 'flex';
-  const toggleBtnClose = document.querySelector('.toggle_btn,.toggle-btn i');
-  toggleBtnClose.style.display = 'none';
-  const CloseBtn = document.querySelector('.close_btn,.close_btn i');
-  CloseBtn.style.display = 'block';
-}
-
-function hideDropdown() {
-  const Dropdown = document.querySelector('.dropdown_menu');
-  Dropdown.style.display = 'none';
-  const toggleBtnClose = document.querySelector('.toggle_btn,.toggle-btn i');
-  toggleBtnClose.style.display = 'block';
-  const CloseBtn = document.querySelector('.close_btn,.close_btn i');
-  CloseBtn.style.display = 'none';
-}
-
 // cart alert
 
 function loginAlert() {
